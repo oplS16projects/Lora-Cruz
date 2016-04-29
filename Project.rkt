8b227c0ffff4366ebb71d74de16303a196279625
Andry <andrylr@gmail.com>
2016-04-29 01:49:58 -0400

    updated gui and heal

--- Project.rkt
+++ Project.rkt
@@ -1,41 +1,63 @@
 #lang racket/gui
 
-(define PlayerHp 100)
-(define StarterMonsterHp (random 25))
+(define StarterMonsterHp 50)
 (define BossHp 1000)
 
 (define frame (new frame% [label "Welcome to Dungeon Hall Z"]
-                   [min-width 200]
-                   [min-height 150]
+                   [min-width 625]
+                   [min-height 300]
                    ))
 (define my-font (make-object font% 12 'modern))
 (define text (new text-field%
-                  [label ""]
+                  [label "Player:"]
+                 
                   [min-height 50]
-                  [min-width 258]
+                  [min-width 700]
                   [parent frame]
                   [font my-font]
+                  [stretchable-width #f]
                   [enabled #f]))
 
 
 (define monster-font (make-object font% 12 'modern))
 (define monster-text (new text-field%
-                  [label ""]
+                  [label "Monster:"]
                   [min-height 50]
-                  [min-width 258]
+                  [min-width 700]
                   [parent frame]
                   [font my-font]
+                  [stretchable-width #f]
                   [enabled #f]))
+
+
+
+
+
+
+(define potion-font (make-object font% 12 'modern))
+(define potion-text (new text-field%
+                  [label "Potions"]
+                  [min-height 30]
+                  [min-width 350]
+                  [parent frame]
+                  [font my-font]
+                  [stretchable-width #f]
+                  [enabled #f]))
+
 
 
 (define editor (send text get-editor))
 (define monster-editor (send  monster-text get-editor))
+(define potion-editor (send  potion-text get-editor))
 
 (define (clear-text)
   (send editor delete 0 (string-length (send editor get-text))))
 
 (define (monster-clear-text)
   (send monster-editor delete 0 (string-length (send monster-editor get-text))))
+
+(define (potion-clear-text)
+  (send potion-editor delete 0 (string-length (send potion-editor get-text))))
  
  
 
@@ -43,56 +65,96 @@
  
  
 (send editor set-padding
-      225 ;left padding
+      0 ;left padding
       0 ;top padding
       0 ;right padding
       0 ;bottom padding
       )
 
 (send monster-editor set-padding
-      225 ;left padding
+      0 ;left padding
       0 ;top padding
       0 ;right padding
       0 ;bottom padding
       )
- 
+
+(send potion-editor set-padding
+      0 ;left padding
+      0 ;top padding
+      0 ;right padding
+      0 ;bottom padding
+      )
 
 (define msg (new message% [parent frame]
                           [label ""]))
 
 
   
+(define monster-dead "Enemy defeated")
+(define player-dead "YOU DIED, WORTHLESS SCUM!")
 
+(define (monster-attack-output HP damage)
+  (string-append "Monsters HP: " (->string HP) " Monster attacked for: "  (->string damage) " damage" ))
+
+(define (player-attack-output HP damage)
+  (string-append "Players HP: " (->string HP)  " You attacked for: "  (->string damage) " damage" ))
+
+(define (player-heal-output HP damage)
+(string-append "Players HP: " (->string HP)  " You Healed for: "  (->string "50") " health" ))
+
+
+
+
+
+
+(define (nextPhase player-HP monster-HP player-damage-done monster-damage-done)
+  (send editor insert (player-attack-output player-HP player-damage-done))
+  (send monster-editor insert  (monster-attack-output monster-HP monster-damage-done)))
+
+(define (healPhase player-HP monster-HP player-healing-done monster-damage-done)
+  (send editor insert  (player-heal-output player-HP monster-damage-done))
+  (send monster-editor insert  (monster-attack-output monster-HP monster-damage-done)))
 
 
 (define battle (new horizontal-panel% [parent frame]))
+
 (new button% [parent battle]
              [label "Attack"]
-             [min-width 100]
+             [min-width 130]
              [min-height 100]
-             [callback (lambda (b e) (clear-text)
+             [callback (lambda (b e) (clear-text)    
                          (monster-clear-text)
-                             (define damage-done (->string ((Player1 'attack)1)))
-                             (define attack-output(string-append "You attacked for: "  damage-done " damage" ))
-                         (define monster-damage-done (->string ((monster 'attack)100)))
-                         (define monster-attack-output(string-append "Monster attacked for: "  monster-damage-done " damage" ))
-                             (send editor insert attack-output)
-                         (send monster-editor insert  monster-damage-done))])
+                         (define monster-damage-done (monster 'attack))
+                         (define player-damage-done (Player1 'attack))  
+                         (define monster-damage-taken ((monster 'attacked) player-damage-done))
+                         (define player-damage-taken ((Player1 'attacked) monster-damage-done))
+                         (define player-HP (Player1 'getHealth))
+                         (define monster-HP (monster 'getHealth))
+                         (cond ((< player-HP 0)(send editor insert player-dead))
+                               ((< monster-HP 0)(send editor insert monster-dead))
+                               (else(nextPhase player-HP monster-HP player-damage-done monster-damage-done)))
+                         )])
                  
 
 (new button% [parent battle]
              [label "Heal"]
-             [min-width 100]
+             [min-width 130]
              [min-height 100]
 
  [callback (lambda (b e) (clear-text)
-             (monster-clear-text)
-                             (define healing-done (->string ((Player1 'heal)100)))
-                             (define healing-output(string-append  "You healed for: "  "20" " Health"))
-              (define monster-damage-done (->string ((monster 'attack)100)))
-                         (define monster-attack-output(string-append "Monster attacked for: "  monster-damage-done " damage" ))
-                             (send editor insert healing-output)
-             (send monster-editor insert  monster-damage-done))])
+                         (monster-clear-text)
+                         (potion-clear-text)
+                         (define monster-damage-done (monster 'attack))                     
+                         (define player-healing-done (->string(Player1 'healed)) )
+                         (define player-healing-taken(string-append  "You healed for: " "50" " Health"))
+                         (define player-damage-taken ((Player1 'attacked) monster-damage-done))
+                         (define player-HP (Player1 'getHealth))
+                         (define monster-HP (monster 'getHealth))
+                          (send potion-editor insert player-healing-done)
+                             (cond ((< player-HP 0)(send editor insert player-dead))
+                                   ((< monster-HP 0)(send editor insert monster-dead))
+                                   (else(healPhase player-HP monster-HP player-healing-done monster-damage-done)))
+          )])
                  
 
 
@@ -102,7 +164,7 @@
 (new button%
              [parent battle]
              [label "Run"]
-             [min-width 100]
+             [min-width 130]
              [min-height 100]
             [callback (lambda (b e) (clear-text)
                         (monster-clear-text)
@@ -120,7 +182,7 @@
 
 (new button% [parent battle]
              [label "Special"]
-             [min-width 100]
+             [min-width 130]
              [min-height 100]
             [callback (lambda (b e) (clear-text)
                         (monster-clear-text)
@@ -134,15 +196,41 @@
 
 
 
+(new button% [parent battle]
+             [label "Start/Restart"]
+             [min-width 130]
+             [min-height 100]
+
+ [callback (lambda (b e) 
+                         (clear-text)
+                         (monster-clear-text)
+                         (potion-clear-text)
+                         (getHealed)
+                         (define start-health(string-append "Players HP: " "100" ))
+                         (define monster-start-health(string-append "Monster HP: " "50" ))
+                         (define start-potions(string-append  "11" ))
+                         (send editor insert start-health)
+                         (send monster-editor insert monster-start-health)  
+                         (send potion-editor insert  start-potions)
+          )])
+                 
+
+
 
 (send frame show #t)
-
+(define health 100)
+(define (getHealth)
+      health)
 
  ;;Creation of Player
 (define (Player health potions mana)
   ;defining miss chance
   (define special-counter 0)
+  (define PlayerHP health)
+  (define PlayerPotion potions)
+
   
+
   (define special special-counter) 
    (if (= special-counter 3)
        ;;remove monster / damage boss to half
@@ -152,27 +240,25 @@
 
   ;Damage taken after attacking
   ;;change to monster health instead of player
-  (define (attack health)
+  (define (attack)
     (define miss (random 100))
+    (define damage (random 30))
     (if ( >= miss 30)
-        (if (>= health 0)
-        (begin
-          (+ special 1)
-          (set! health (- health (random 15)))
-               health)
-        "You are Dead")
-        "Player Missed"))
-  ;Healing
-  (define (heal health potion)
-    (if(> potion 0)
-    (set! health (+ health 20))
-    health)"You Cannot Heal")
-  ;Also Called when healing
-  (define (potionuse potion)
-   (if (> potion 0)
-        (begin (set! potion (- potion 1))
-               potion)
-        "No More Potions"))
+          damage
+          0))
+  
+  ;;player gets attacked
+  (define (getAttacked damage)
+    (set! PlayerHP (- PlayerHP damage)))
+
+
+  (define (getHealed)
+    
+    (set! PlayerHP (+ PlayerHP 50))
+    (set! PlayerPotion (- PlayerPotion 1))
+    PlayerPotion)
+  
+   
   ;When you run Away you 
   (define (run potion)
     (define drop (random 100))
@@ -182,13 +268,18 @@
                potion)
         "You Stumbled as you ran")
         "You got Away Safely"))
- 
+
+  (define (setHealth health)
+    (set! PlayerHP health))
+  
   (define (dispatch m)
-    (cond ((eq? m 'attack) attack)
-          ((eq? m 'heal) heal potionuse)
+    (cond ((eq? m 'attack) (attack))
           ((eq? m 'run) run)
           ((eq? m 'special) special)
-          
+          ((eq? m 'getHealth) PlayerHP)
+          ((eq? m 'setHealth) setHealth)
+          ((eq? m 'attacked) getAttacked)        
+          ((eq? m 'healed) (if (and (> PlayerPotion 0) (> PlayerHP 0)) (getHealed)"No Potions"))
          ;;Gave an error because dispatch requires an else
           (else (error "Not a Command"
                        m))))
@@ -199,19 +290,23 @@
 
 ;;Creation of Monsters
 (define (Monster health)
+  (define MonsterHP health)
   ;Damage taken after attacking
   ;;change to player health instead of monster
-  (define (attack health)
+  (define (attack)
     (define miss (random 100))
-    (if ( >= miss 60)
+    
         (if (>= health 0)
-        (begin (set! health (- health (random 30)))
-               health)
-        "Monster is dead")
-        "Monster Missed"))
+            (random 30)
+            0))
 
+      (define (getAttacked damage)
+            (set! MonsterHP (- MonsterHP damage)))
+    
   (define (dispatch m)
-    (cond ((eq? m 'attack) attack)
+    (cond ((eq? m 'attack) (attack))
+          ((eq? m 'getHealth) MonsterHP)
+          ((eq? m 'attacked) getAttacked)
        ;;Gave an error because dispatch requires an else
           (else (error "Not a Command"
                        m))))
@@ -221,13 +316,16 @@
 ;;manipulate these when moving up and down the tree
 
 ;;If Tree level is lowest create player
-(define Player1 (Player PlayerHp 0 100))
+(define Player1 (Player 100 11 100))
 
-;;Store New Hp for reuse
-(define StoredHp 0)
-(if (number? ((Player1 'attack)100))
-(set! StoredHp((Player1 'attack)100))
-"Needs a Number")
+;;starting
+;;(define player-start)
+;;(Player 100 11 100)
+(define (getHealed)
+    (Player 100 11 100))
+  
+
+
 
 
 ;;Each time a new tree Level is hit create a monster
@@ -245,23 +343,4 @@
      (display x out))))
 
 
-
-
-
-
-
-
 ;;(define damage-done (->string ((Player1 'attack)100)))
-
-
-
-;;Monster will always attack except when you run
-(define monster-damage-done (->string ((monster 'attack)100)))
-(define monster-attack-output(string-append "Monster attacked for: "  monster-damage-done " damage" ))
-
-
-
-
-
-
-
